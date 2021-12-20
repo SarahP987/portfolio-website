388bdc82420209ff8c988030235e410c3e2948ec
Sarah <sarah.pearson006@gmail.com>
2021-12-20 16:38:58 +1300

    added js and toggled menu

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,2 +1,6 @@
-<script src="js/scripts.js"></script>
 console.log( "Hello, this is my portfolio. I hope you like it.")
+(function ($){
+  $('#hamburgerMenu').click (function (event){
+    $('#menu').toggleClass('menu-visible');
+  });
+})($);
