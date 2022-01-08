d5315448dc55e1ffb63bdbaa5e0ecd9625fc9fad
Sarah <sarah.pearson006@gmail.com>
2022-01-09 12:01:16 +1300

    Linting Changes
    Linting Changes

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,6 +1,5 @@
-console.log( "Hello, this is my portfolio. I hope you like it.")
 (function ($){
-  $('#hamburgerMenu').click (function (event){
-    $('#menu').toggleClass('menu-visible');
+  $('#hamburgerMenu').click(function (event){
+    $('.toggle').toggleClass('responsive');
   });
 })($);
